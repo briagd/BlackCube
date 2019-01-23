e55e231cc4b4f4c88a799fd623a90205c9fb9329
MoToKo - Briag <briagd@yahoo.com>
2019-01-23 13:40:30 -0600

    Added jump, particle effect, moving camera

--- BlackCube/Assets/Scripts/PlayerControl.cs
+++ BlackCube/Assets/Scripts/PlayerControl.cs
@@ -8,26 +8,71 @@ public class PlayerControl : MonoBehaviour
     public float speed;
     public float jumpForce;
 
+    public GameObject jumpEffect;
+
     //private
     float moveInput;
+    bool facingRight = true;
     Rigidbody2D rb;
 
+    //checking if is on ground
+    bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    public int extraJumpValue;
+    private int extraJump;
 
     // Start is called before the first frame update
     void Start()
     {
+        extraJump = extraJumpValue;
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isGrounded == true)
+        {
+            extraJump = extraJumpValue;
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && extraJump > 0)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+            Instantiate(jumpEffect, transform.position, Quaternion.identity);
+            extraJump--;
+        } else if (Input.GetKeyDown(KeyCode.UpArrow) && extraJump > 0 && isGrounded)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+            Instantiate(jumpEffect, transform.position, Quaternion.identity);
+
+        }
     }
 
     private void FixedUpdate()
     {
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
         moveInput = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(moveInput * speed * Time.deltaTime, rb.velocity.y);
+        if(facingRight == false && moveInput > 0)
+        {
+            Flip();
+        } else if (facingRight == true && moveInput < 0)
+        {
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
     }
 }
